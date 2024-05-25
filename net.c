f72215b047ba3e713153e50db7f9ee8c8c6ec733
YAMAMOTO Masaya <pandax381@gmail.com>
2024-05-26 05:31:47 +0900

    Skeleton of step2

--- net.c
+++ net.c
@@ -86,6 +86,11 @@ net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, si
 
 // プロトコルスタックを初期化
 int
+net_input(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev)
+{
+}
+
+int
 net_init(void)
 {
     // INFOレベルのログメッセージを出力するマクロ
